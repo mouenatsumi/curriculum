1c474baf8f611023baad258ffd9f9818f7d6d416
mouenatsumi <n7230823@gmail.com>
2021-05-20 21:03:52 +0900

    配列
    課題提出

--- 2-7/配列.php
+++ 2-7/配列.php
@@ -1,5 +1,8 @@
 <?php
 $data = ["red" => "赤", "blue" => "青", "green" => "緑"];
+var_dump($data);
+echo '<br>';
+$data = ["red" => "赤", "blue" => "青", "green" => "緑"]; 
 $data["yello"]= "黄色";
 var_dump($data)
 ?>
